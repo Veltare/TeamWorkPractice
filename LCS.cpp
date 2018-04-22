6dc24d58665c6f215b562fb21bc204d89f0a3565
Veltare <Veltare@gmail.com>
2018-04-22 14:11:45 +0700

    add LCS Algorithm(not work)

---
+++ LCS.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LCS.h"
+
+
+void add(LCS_Subsequence addArg,LCS_Subsequence *obj)
+	{
+	    LCS_Subsequence*temp;
+	    addArg.next = obj;
+	    obj = &addArg;
+	    addArg.size = addArg.size + 1;
+	}
+void print(LCS_Subsequence addArg,LCS_Subsequence *obj)
+	{
+	    LCS_Subsequence*temp = obj;
+	    printf("\n");
+	    while(temp->next != NULL)
+	    {
+		printf("%c",temp->symbol);
+		temp->head = temp->next;
+	    }
+	}
+void LCS(char *S1,char *S2,int n,int m,int **L)    
+{
+    Subsequence *newArg = (Subsequence*)malloc(sizeof(Subsequence));
+    for(int i = n;i>=0;i--)
+        for(int j = m;j>=0;j--)
+        {
+            if(S1[i]=='/0' || S2[j] == '/0')
+                L[i][j] = 0;
+            //if(i == 0|| j == 0)
+                //L[i][j] = 0;    
+            else    
+            if(S1[i] == S2[j])
+                L[i][j] = 1 + L[i+1][j+1];
+            else
+                L[i][j] = MAX(L[i+1][j],L[i][j+1]);
+        }
+    for(int i = 0,j = 0;L[i][j]!=0 && i < n && j < m;)
+    {
+
+        if(S1[i] == S2[j])
+        {
+            newArg->symbol = S1[i];
+            newArg->add(*newArg,subOne);
+            i+=1;
+            j+=1;
+            newArg = (Subsequence*)malloc(sizeof(LCS_Subsequence));
+        }
+        else
+        {
+            if(L[i][j] == L[i+1][j])
+            {
+                i+=1;
+            }
+            else
+            {
+                j+=1;
+            }
+        }
+    }    
+}
---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LCS.h"
+
+main()
+{
+    FILE *sqc1,*sqc2;
+    char *text1,*text2;
+    int text_size1 = 0;
+    int text_size2 = 0;
+    int **L;
+    sqc1 = fopen("sequenceOne.txt","r");
+    sqc2 = fopen("sequenceTwo.txt","r");
+
+    fseek(sqc1,0,SEEK_END);
+    fseek(sqc2,0,SEEK_END);
+
+    text_size1 = ftell(sqc1);
+    text_size2 = ftell(sqc2);
+
+    text1 = (char*)malloc(text_size2);
+    text2 = (char*)malloc(text_size1);
+    L = (int**)malloc(text_size1*sizeof(int*));
+    for(int i = 0;i<text_size1;i++)
+    {
+        L[i] = (int*)malloc(text_size2*sizeof(int));
+    }
+
+    fseek(sqc1,0,SEEK_SET);
+    fseek(sqc2,0,SEEK_SET);
+    
+    while(fscanf(sqc1,"%s",text1)!=EOF)
+    {
+        printf("\n%s",text1);
+    }
+    while(fscanf(sqc2,"%s",text2)!=EOF)
+    {
+        printf("\n%s",text2);
+    }
+    LCS(text1,text2,text_size1-2,text_size2-1,L);
+    printf("\n");
+    for(int i = 0;i<text_size1;i++)
+    {
+        for(int j = 0;j<text_size2;j++)
+        {
+           printf("%d",L[i][j]);
+        }
+        printf("\n");
+    }
+    //subOne->print(subOne);
+    return 0;
+}
